387bda007b2733cf20cdcfc5a468efab9942ebb2
schooolboy <logoshin01@mail.ru>
2022-06-03 00:31:10 +0500

    first3

--- TableSprApplication.java
+++ TableSprApplication.java
@@ -1,11 +1,11 @@
-package com.nikita.dem;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.nikita.dem"})
+@ComponentScan(basePackages = {"com.example.demo"})
 public class TableSprApplication {
 
 	public static void main(String[] args) {
